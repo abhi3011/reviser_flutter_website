09a7a6bc41a0fce92f480e1c7bc9819ee7ca5830
abhilash <cabhi3011@gmail.com>
2020-11-22 10:32:20 +0530

    intial commit

---
+++ lib/constants/pallete.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// app base colours
+const Color primaryDarkest = Color(0xFF2fa769);
+const Color intermediate1 = Color(0xFF39ab6f);
+const Color intermediate2 = Color(0xFF43ae76);
+const Color primaryLightest = Color(0xFF4cb37c);
+
+// text and high light colours
+const Color primaryHighlight = Color(0xFFebf6ee);
+const Color white = Colors.white;
+const Color primaryTextColor = Color(0xFF343d66);
+
+// to do colours
+const Color todoColour1 = Color(0xFF343d66);
+const Color todoColour2 = Color(0xFF2fa769);
+const Color todoColour3 = Color(0xFFf44234);
+const Color todoColour4 = Color(0xFF00bbd4);
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_website/constants/pallete.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final homeContainerKey = new GlobalKey();
+  final missionContainerKey = new GlobalKey();
+  final aboutUsContainerKey = new GlobalKey();
+  final contactContainerKey = new GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: primaryHighlight,
+            height: 60,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  height: 50,
+                  child: Image(image: AssetImage('images/logo.jpg')),
+                ),
+                Spacer(),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        12.0,
+                        0.0,
+                        12.0,
+                        0.0,
+                      ),
+                      child: InkWell(
+                        child: Container(
+                          child: Text('Home'),
+                        ),
+                        onTap: () {
+                          Scrollable.ensureVisible(homeContainerKey.currentContext);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        12.0,
+                        0.0,
+                        12.0,
+                        0.0,
+                      ),
+                      child: InkWell(
+                        child: Container(
+                          child: Text('Mission'),
+                        ),
+                        onTap: () {
+                          Scrollable.ensureVisible(missionContainerKey.currentContext);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        12.0,
+                        0.0,
+                        12.0,
+                        0.0,
+                      ),
+                      child: InkWell(
+                        child: Container(
+                          child: Text('About us'),
+                        ),
+                        onTap: () {
+                          Scrollable.ensureVisible(aboutUsContainerKey.currentContext);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        12.0,
+                        0.0,
+                        12.0,
+                        0.0,
+                      ),
+                      child: InkWell(
+                        child: Container(
+                          child: Text('Contact'),
+                        ),
+                        onTap: () {
+                          Scrollable.ensureVisible(contactContainerKey.currentContext);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  key: homeContainerKey,
+                  decoration: BoxDecoration(color: primaryDarkest),
+                  height: 550,
+                ),
+                Container(
+                  key: missionContainerKey,
+                  decoration: BoxDecoration(color: primaryTextColor),
+                  height: 550,
+                ),
+                Container(
+                  key: aboutUsContainerKey,
+                  decoration: BoxDecoration(color: primaryHighlight),
+                  height: 550,
+                ),
+                Container(
+                  key: contactContainerKey,
+                  decoration: BoxDecoration(color: todoColour4),
+                  height: 550,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
